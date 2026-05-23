9b8457f78d4a07723fb600d3d0ae9e3cf564190a
Bam-Manu-182 <bammanuel182@gmail.com>
2026-05-23 10:19:50 -0500

    Project 207 V1.1.9.2
    Modificacion app.py por respuesta en servidor al momento dé recepción de link para analizar.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Bam-Manu-182/python-project-207/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Bam-Manu-182/python-project-207/actions)
+
+Link de Render.com como pureba de funcion de entorno de producción:
+https://python-project-207-32p0.onrender.com
+
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template
 import os
-from dotenv import load_dotenv
 
-
-load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev_secret_key_default')
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='¡Hola, Flask!')
+    return render_template('index.html', title='Hello, Flask!')
 
 if __name__ == '__main__':
     app.run()
