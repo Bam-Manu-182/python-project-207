d1f5559879565f79e0f432607e35c3e0d3c07d65
Bam-Manu-182 <bammanuel182@gmail.com>
2026-06-17 18:54:23 -0500

    Project 207 V4 Commit 3
    Paso 2. Se crea la plantilla index.html básica según la guía. Además se corrige el archivo para que tenga solo el saludo inicial con Bootstrap por CDN. Paso completo #2

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, render_template
+from dotenv import load_dotenv
 
+load_dotenv()
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '¡Hola Flask!'
+    return render_template('index.html')
